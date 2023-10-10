d2ea3a6018949d549d39f6434e42cd7b22a0eb9d
Sharath-44 <sharath.shashidhara@gmail.com>
2023-10-10 23:30:19 +0530

    Added app_bar_widget Sharath M S

--- lib/models/widget_model.dart
+++ lib/models/widget_model.dart
@@ -3,11 +3,15 @@ class WidgetModel {
   final String name;
   final String description;
   final String imagePath;
+  final String outputImage;
+  final String codeSnippet;
   //can add more attributes here, make sure to intialize them in the constructor
 
   WidgetModel({
     required this.name,
     required this.description,
     required this.imagePath,
+    required this.outputImage,
+    required this.codeSnippet,
   });
 }
