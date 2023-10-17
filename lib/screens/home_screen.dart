fdcfbc95b179a5c0697deddcdcf6b1ef41e2552c
Sharath-44 <sharath.shashidhara@gmail.com>
2023-10-17 18:37:31 +0530

    Changed and organized HS and WS - Sharath M S

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,28 +1,82 @@
 import 'package:flutter/material.dart';
 import '/models/widget_model.dart';
 import '/data/widget_data.dart';
-import 'package:flutter_folio/widgets/app_bar_widget.dart';
-import 'package:flutter_folio/widgets/scaffold_widget.dart';
+import '/widgets/app_bar_widget.dart';
+import '/widgets/scaffold_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
-      appBar: CustomAppBar(
-        title: 'FlutterFolio Widget Catalog',
+      appBar: AppBar(
+        title: Text('FlutterFolio Widget Catalog'),
+        backgroundColor: Color(0xFF5078B2),  // Change color of AppBar
       ),
-      body: ListView.builder(
-        itemCount: widgetData.length,
-        itemBuilder: (context, index) {
-          final widgetModel = widgetData[index];
-          return ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/widgetDetail',
-                  arguments: widgetModel);
-            },
-            child: Text(widgetModel.name),
-          );
-        },
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.blue[100]!, Colors.blue[400]!],
+          ),
+        ),
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, 
+            childAspectRatio: 3/2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: widgetData.length,
+          itemBuilder: (context, index) {
+            final widgetModel = widgetData[index];
+            return GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, '/widgetDetail', arguments: widgetModel);
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.vertical(top: Radius.circular(15)),
+                        child: Image.asset(
+                          widgetModel.imagePath,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        widgetModel.name,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'YourCustomFont',
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,40 +2,53 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData lightTheme() {
-    final Color primaryColor = Color(0xFF1565C0); // A shade of blue
-    final Color accentColor =
-        Color(0xFF64B5F6); // A lighter shade of blue for accents
+    return themeData(Brightness.light);
+  }
+
+  static ThemeData darkTheme() {
+    return themeData(Brightness.dark);
+  }
+
+  static ThemeData themeData(Brightness brightness) {
+    final bool isDark = brightness == Brightness.dark;
+
+    final Color primaryColor = Color(0xFF1565C0);
+    final Color accentColor = Color(0xFF64B5F6);
+
+    final Color foregroundColor = isDark ? Colors.white : Colors.black;
+    final Color backgroundColor = isDark ? Colors.black87 : Color(0xFFFAFAFA); // Off-white for light mode
 
     final ColorScheme colorScheme = ColorScheme(
-        brightness: Brightness.light,
-        primary: primaryColor,
-        secondary: accentColor,
-        error: Colors.red, // Specify your error color here
-        onPrimary: Colors.white,
-        onSecondary: Colors.black,
-        surface: Colors.white, // Background color
-        background: Colors.white,
-        onSurface: Colors.black, // Text color on the background
-        onBackground: Colors.black,
-        onError: Colors.white);
+      brightness: brightness,
+      primary: primaryColor,
+      secondary: accentColor,
+      background: backgroundColor,
+      surface: backgroundColor,
+      error: Colors.red,
+      onPrimary: foregroundColor,
+      onSecondary: foregroundColor,
+      onBackground: foregroundColor,
+      onSurface: foregroundColor,
+      onError: isDark ? Colors.black : Colors.white,
+    );
 
     return ThemeData(
       primaryColor: primaryColor,
       colorScheme: colorScheme,
-      brightness: Brightness.light, // Specify the brightness
-      scaffoldBackgroundColor: Colors.white, // White background
+      brightness: brightness,
+      scaffoldBackgroundColor: backgroundColor,
       textTheme: TextTheme(
-        bodyText1: TextStyle(fontSize: 16, color: Colors.black), // Black text
+        bodyText1: TextStyle(fontSize: 16, color: foregroundColor),
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ButtonStyle(
           backgroundColor: MaterialStateProperty.all(primaryColor),
-          foregroundColor: MaterialStateProperty.all(Colors.white),
+          foregroundColor: MaterialStateProperty.all(foregroundColor),
         ),
       ),
       appBarTheme: AppBarTheme(
         backgroundColor: primaryColor,
-        foregroundColor: Colors.white,
+        foregroundColor: foregroundColor,
       ),
     );
   }
