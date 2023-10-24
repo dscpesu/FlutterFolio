661174fa323cc41a3fd253e39b9784ce7c21e349
Sharath-44 <sharath.shashidhara@gmail.com>
2023-10-24 23:21:45 +0530

    Added 3 more widgets

--- lib/data/widget_data.dart
+++ lib/data/widget_data.dart
@@ -3,11 +3,12 @@ import 'package:flutter_folio/models/widget_model.dart';
 
 List<WidgetModel> widgetData = [
   WidgetModel(
-    name: 'Image',
-    description: 'The Image widget in Flutter is a fundamental component used to display images within an application. It provides a seamless way to incorporate visual content, such as icons, photographs, or graphics, into the user interface.',
-    imagePath: 'assets/images/image/image.png',
-    outputImage: 'assets/images/image/image_op.png',
-    codeSnippet: '''import 'package:flutter/material.dart';
+      name: 'Image',
+      description:
+          'The Image widget in Flutter is a fundamental component used to display images within an application. It provides a seamless way to incorporate visual content, such as icons, photographs, or graphics, into the user interface.',
+      imagePath: 'assets/images/image/image.png',
+      outputImage: 'assets/images/image/image_op.png',
+      codeSnippet: '''import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -154,7 +155,8 @@ class _ScaffoldExampleState extends State<ScaffoldExample> {
 '''),
   WidgetModel(
       name: 'Container',
-      description: 'A convenience widget that combines common painting, positioning, and sizing widgets. A container first surrounds the child with padding (inflated by any borders present in the decoration) and then applies additional constraints to the padded extent (incorporating the width and height as constraints, if either is non-null). The container is then surrounded by additional empty space described from the margin.',
+      description:
+          'A convenience widget that combines common painting, positioning, and sizing widgets. A container first surrounds the child with padding (inflated by any borders present in the decoration) and then applies additional constraints to the padded extent (incorporating the width and height as constraints, if either is non-null). The container is then surrounded by additional empty space described from the margin.',
       imagePath: 'assets/images/container/container.png',
       outputImage: 'assets/images/container/container_op.png',
       codeSnippet: '''
@@ -165,7 +167,98 @@ class _ScaffoldExampleState extends State<ScaffoldExample> {
           width: 48.0,
           height: 48.0,
           ),  
-      )'''
-  )
+      )'''),
+  WidgetModel(
+      name: 'ElevatedButton',
+      description:
+          'Use elevated buttons to add dimension to otherwise mostly flat layouts, e.g. in long busy lists of content, or in wide spaces. Avoid using elevated buttons on already-elevated content such as dialogs or cards.',
+      imagePath: 'assets/images/elevatedButton/elevatedButton.png',
+      outputImage: 'assets/images/elevatedButton/elevatedButton_op.png',
+      codeSnippet: '''
+      import 'package:flutter/material.dart';
+
+/// Flutter code sample for [ElevatedButton].
+
+void main() => runApp(const ElevatedButtonExampleApp());
+
+class ElevatedButtonExampleApp extends StatelessWidget {
+  const ElevatedButtonExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('ElevatedButton Sample')),
+        body: const ElevatedButtonExample(),
+      ),
+    );
+  }
+}
+
+class ElevatedButtonExample extends StatefulWidget {
+  const ElevatedButtonExample({super.key});
+
+  @override
+  State<ElevatedButtonExample> createState() => _ElevatedButtonExampleState();
+}
+
+class _ElevatedButtonExampleState extends State<ElevatedButtonExample> {
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ElevatedButton(
+            style: style,
+            onPressed: null,
+            child: const Text('Disabled'),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            style: style,
+            onPressed: () {},
+            child: const Text('Enabled'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+'''),
+  WidgetModel(
+      name: 'Text',
+      description:
+          'The Text widget displays a string of text with single style. The string might break across multiple lines or might all be displayed on the same line depending on the layout constraints.',
+      imagePath: 'assets/images/text/text.png',
+      outputImage: 'assets/images/text/text_op.png',
+      codeSnippet: '''
+      Text(
+  'Hello, _name! How are you?',
+  textAlign: TextAlign.center,
+  overflow: TextOverflow.ellipsis,
+  style: const TextStyle(fontWeight: FontWeight.bold),
+)'''),
+WidgetModel(
+      name: 'Column',
+      description:
+          'The Column widget does not scroll (and in general it is considered an error to have more children in a Column than will fit in the available room). If you have a line of widgets and want them to be able to scroll if there is insufficient room, consider using a ListView.',
+      imagePath: 'assets/images/column/column.png',
+      outputImage: 'assets/images/column/column_op.png',
+      codeSnippet: '''
+      const Column(
+  children: <Widget>[
+    Text('Deliver features faster'),
+    Text('Craft beautiful UIs'),
+    Expanded(
+      child: FittedBox(
+        child: FlutterLogo(),
+      ),
+    ),
+  ],
+)'''),
   // Add more widgets here
 ];
---
+++ lib/widgets/column_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+enum ColumnType { spaced, centered }
+
+class CustomColumn extends StatelessWidget {
+  final List<Widget> children;
+  final ColumnType type;
+
+  CustomColumn({required this.children, this.type = ColumnType.spaced});
+
+  @override
+  Widget build(BuildContext context) {
+    MainAxisAlignment alignment;
+    double spacing;
+
+    switch (type) {
+      case ColumnType.centered:
+        alignment = MainAxisAlignment.center;
+        spacing = 0.0;
+        break;
+      default: // ColumnType.spaced
+        alignment = MainAxisAlignment.start;
+        spacing = 8.0;
+    }
+
+    return Column(
+      mainAxisAlignment: alignment,
+      children: children.map((child) {
+        return Padding(
+          padding: EdgeInsets.symmetric(vertical: spacing / 2),
+          child: child,
+        );
+      }).toList(),
+    );
+  }
+}
